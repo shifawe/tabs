a5d72e0526cb13dbf588b6fa0a5c5dbbe0782149
451154708@qq.com <451154708@qq.com>
2020-04-24 14:26:10 +0800

    添加图片播放模式 。 以及自动播放bug

--- tabs.js
+++ tabs.js
@@ -11,14 +11,8 @@
 				skin: 'tab-default'
 			}
 
-			//如果用户有传值 并且传的是{}或new obj的东西
-			if(options && $.isPlainObject(options)){
-				$.extend(this.options, options);
-			}else{
-				//没有传值的时候 设置一个状态
-				this.isoptions = true;
-			}
-
+			$.extend(this.options, options);
+		
 			var _options = this.options;
 			var self = $(this);
 			var bd_li = self.find('.bd>.item');
@@ -27,7 +21,7 @@
 
 			_options.onindex = (_options.onindex > bd_li.length || _options.onindex < 1) ? bd_li.length : _options.onindex;
 
-			for (let i = 0; i < bd_li.length; i++) {
+			for (var i = 0; i < bd_li.length; i++) {
 				// 在没有传入列表时 设定默认列表头
 				if(_options.list.length == 0 || _options.list.length != bd_li.length){
 					_options.list.push('默认Tab'+i);
@@ -57,14 +51,13 @@
 				delay($(this), _options.timeout);
 			});
 
-			var autoPlay = function(idx, action){
+			var play = function(idx, action){
 				var current = hd.find('li.on');
 				var firstItem = hd.find('li.item').eq(0);
 				var len = hd.find('li.item').length;
 				var lastItem = hd.find('li.item').eq(len-1);
-				var index = current.index() + idx;
+				var index = current.index() + (idx-0);
 
-	
 				// var child = index === 0 ? lastItem : firstItem;
 				// var item = index === len ? child : current[action]('li');
 				// var bd_i = index === 0 ? len : index;
@@ -91,32 +84,35 @@
 			}
 
 			// 自动切换
-			var start = function(){
-				timer = setInterval( function(){autoPlay(1, 'next')}, _options.autoplay );
+			function start(){
+				timer = setInterval(function(){
+					play(1, 'next');
+				}, _options.autoplay );
 			}
 
 			if(_options.arrow == true) {
-				var prev = $('<span class="prev">＜</span>');
-				var next = $('<span class="next">＞</span>');
-				self.append(prev);
-				self.append(next);
+				var btn_prev = $('<span class="prev">＜</span>');
+				var btn_next = $('<span class="next">＞</span>');
+				self.append(btn_prev);
+				self.append(btn_next);
 
-				prev.on('click', function(){
-					autoPlay(-1, 'prev')
+				btn_prev.on('click', function(){
+					play(-1, 'prev')
 				});
-				next.on('click', function(){
-					autoPlay(1, 'next')
+				btn_next.on('click', function(){
+					play(1, 'next')
 				})
 
 			}
 
 			// 当用户传了 autoplay 值后
 			if(!!options.autoplay){
-				start()
+				console.log(3)
+				start();
 				self.hover(function(){
-					clearInterval(timer)
+					clearInterval(timer);
 				}, function(){
-					start()
+					start();
 				})
 			}
 
